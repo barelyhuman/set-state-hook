8ce3294a207aa39fadd7188487b4a84d6a54a52d
Siddharth <ahoy@barelyhuman.dev>
2021-08-23 12:00:34 +0530

    tests: add

---
+++ tests/useSetState.test.js
@@ -0,0 +1,73 @@
+import { test } from 'uvu'
+import * as assert from 'uvu/assert'
+import { useSetState } from '../dist'
+import * as React from 'react'
+import { render } from '@testing-library/react'
+
+test('check if state is readable', async () => {
+  function Component () {
+    const [state, setState] = useSetState({
+      name: 'Reaper',
+      age: 16
+    })
+
+    return (
+      <>
+        <p>name: {state.name}</p>
+        <p>age: {state.age}</p>
+      </>
+    )
+  }
+
+  const { findByText } = render(<Component />)
+  assert.ok(await findByText('name: Reaper'))
+})
+
+test('check if a single state changed', async () => {
+  function Component () {
+    const [state, setState] = useSetState({
+      name: 'Reaper',
+      age: 16
+    })
+
+    React.useEffect(() => {
+      setState({
+        name: 'Hello World'
+      })
+    }, [setState])
+
+    return (
+      <>
+        <p>name: {state.name}</p>
+        <p>age: {state.age}</p>
+      </>
+    )
+  }
+
+  const { findByText } = render(<Component />)
+  assert.ok(await findByText('name: Hello World'))
+})
+
+// Not related to the project, just added to see if the
+// tests fail as expected
+test('fail case check', async () => {
+  function Component () {
+    const [state] = useSetState({
+      name: 'Reaper',
+      age: 16
+    })
+
+    return (
+      <>
+        <p>name: {state.name}</p>
+        <p>age: {state.age}</p>
+      </>
+    )
+  }
+
+  const { findByText } = render(<Component />)
+  const failed = await findByText('name: hello').catch((_) => true)
+  assert.is(failed, true)
+})
+
+test.run()
